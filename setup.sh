49d3f3b7435127b8a348720c52a952e30887b029
Jason Livesay <runvnc@gmail.com>
2023-09-24 12:41:32 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Create base directory
-mkdir -p ~/.jobmgr/output
+USER_HOME=$(eval echo ~$SUDO_USER)  # This gets the home directory of the user who invoked sudo
 
-# Optional: Move the jobmgr.py to a location in the PATH
-cp jobmgr.py /usr/local/bin/jobmgr
+# Create base directory in the user's home, not root's
+mkdir -p $USER_HOME/.jobmgr/output
+
+# Only use sudo for operations that require it
+sudo cp jobmgr /usr/local/bin/jobmgr
